80d7a7a6983a1894ad740907fc9fbf081e69837f
whoIsOneZero <oh.shalom.0@gmail.com>
2023-03-21 04:17:32 +0000

    Sample project, haven't been able to run yet.

---
+++ lib/screens/home.dart
@@ -0,0 +1,41 @@
+import 'package:comeback/constants/app_constants.dart';
+import 'package:comeback/widgets/left_bar.dart';
+import 'package:comeback/widgets/right_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI Calculator",
+          style: TextStyle(
+            color: accentHexColor,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
